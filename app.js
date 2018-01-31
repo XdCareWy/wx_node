377765e77d0ea9cc8d2fc5dd38219c31cd41b8be
ZxdCareWy <aixu159@163.com>
2018-01-31 19:19:57 +0800

    port

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const controller = require('./src/util/controller');
 app.use(bodyParser());
 app.use(controller());
 
-app.listen(3000);
+app.listen(80);
 console.log("server is listenning port 3000!")
