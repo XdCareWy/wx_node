87a4bd2c40e423e90eb50f80f38254510c333d59
ZxdCareWy <aixu159@163.com>
2018-02-07 11:11:26 +0800

    增加https功能

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const Koa = require('koa');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 const controller = require('./src/util/controller');
 
+const key = fs.readFileSync('./cerificate/cert.key');
+const cert = fs.readFileSync('./cerificate/cert.pem')
+
+const PORT = 3000;
+const SSLPORT = 3001;
 
 app.use(bodyParser());
 app.use(controller());
 
-app.listen(3000);
-console.log("server is listenning port 3000!")
+https.createServer({key: key, cert: cert}, app.callback()).listen(SSLPORT);
+app.listen(PORT);
+
+
