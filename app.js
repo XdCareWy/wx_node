d22a6b8797dc800c77aa43e3087ada0f9c41719e
ZxdCareWy <aixu159@163.com>
2018-03-06 12:23:13 +0800

    用koa-body替换body-parser解析

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const Koa = require('koa');
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
-const bodyParser = require('koa-bodyparser');
 const koaLogger = require('koa-logger');
 const convert = require('koa-convert');
+const koaBody = require('koa-body');
 
 const app = new Koa();
 const controller = require('./src/util/controller');
@@ -17,24 +17,9 @@ const SSLPORT = 3001;
 
 // 1. import 'mongodse connect'
 require('./src/util/mongodbConnection');
-// 2. import 'Poetry' Model
-// const Poetry = require('./src/model/poetry');
-// // 3. defined 'Poetry' Entity
-// const poetry = new Poetry({
-// 	url: '1.jpg',
-// 	topic: 'zxd',
-// 	description: 'qazewdsfsdfsfsadfxc'
-// });
-// // 4. CRUL
-// poetry.save(function(err, doc) {
-// 	if(err) {
-// 		console.log('save error: ' + err);
-// 	}
-// 	console.log('save success: \n' + doc);
-// });
 
 app.use(convert(koaLogger()));
-app.use(bodyParser());
+app.use(koaBody({multipart: true}));
 app.use(controller());
 
 try {
