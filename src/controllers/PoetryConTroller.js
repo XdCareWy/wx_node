c8c739c5bc26ef82a9bf7a98afc4701812d60891
ZxdCareWy <aixu159@163.com>
2018-03-09 18:30:05 +0800

    添加删除七牛云图片的功能

--- src/controllers/PoetryConTroller.js
+++ src/controllers/PoetryConTroller.js
@@ -2,15 +2,16 @@ const os = require('os');
 const buildInPath = require('path');
 const fs = require('fs');
 const qiniu = require('../util/qiniu');
-const tool = require('../util/tool');
 const Poetry = require('../model/poetry');
-const logger = require('../util/logger');
+const logger = require('../util/logger').logger;
 
 const find = async (ctx, next) => {
-	const doc = await Poetry.find();
+	const {id} = ctx.query;
+	const doc = id ? await Poetry.find({_id: id}) : await Poetry.find();
 	if(!doc.errors) {
 		ctx.response.body = {code: 1, message: "find ok!", data: doc}
 	}else {
+		console.log(doc)
 		ctx.response.body = {code: 0, message: "find fail!"}
 	}
 }
@@ -25,36 +26,21 @@ const update = async (ctx, next) => {
 }
 
 const remove = async (ctx, next) => {
-	const doc = await Poetry.where({_id: "5a9661df88792c25570049b9"}).remove();
-	if(!doc.errors) {
-		ctx.response.body = {code: 1, message: "delete ok!", data: doc}
-	}else {
-		ctx.response.body = {code: 0, message: "delete fail!"}
-	}
+	// const doc = await Poetry.where({_id: "5a9661df88792c25570049b9"}).remove();
+	// 删除七牛云上图片
+	const res = await qiniu.deleteQiniu('1520567530942.png');
+	console.log(res)
+	// if(!doc.errors) {
+	// 	ctx.response.body = {code: 1, message: "delete ok!", data: doc}
+	// }else {
+	// 	ctx.response.body = {code: 0, message: "delete fail!"}
+	// }
 }
 
 const add = async (ctx, next) => {
-	const {files, fields} = ctx.request.body;
-	const {topic, description} = fields;
-	const {path, name} = files.file;
-	// 创建stream流
-  	const reader = fs.createReadStream(path);
-  	// 使用时间戳重新命名
-  	const fileName = new Date().getTime() + buildInPath.extname(name);
-	const qiniuUploadWrap = tool.wrap(qiniu.uploadQiniu)
- 	const res = await qiniuUploadWrap(reader, fileName);
-
- 	const {statusCode, data} = res[1];
- 	if(statusCode) {
- 		console.log(logger)
- 		console.info(data.key + "上传成功！");
- 		logger.info(data.key + "上传成功！");
- 	}else {
- 		logger.errors(fileName + "上传失败！");
- 	}
-
+	const {url, topic, description} = ctx.request.body;
 	const poetry = new Poetry({
-		url: fileName,
+		url: url,
 		topic: topic,
 		description: description
 	});
@@ -66,10 +52,32 @@ const add = async (ctx, next) => {
 	}
 }
 
+const upload = async (ctx, next) => {
+	const {files, fields} = ctx.request.body;
+	const {path, name} = files.file;
+	// 创建stream流
+	const reader = fs.createReadStream(path);
+	// 使用时间戳重新命名
+	const fileName = new Date().getTime() + buildInPath.extname(name);
+	// 上传
+	const res = await qiniu.uploadQiniu(reader, fileName);
+
+	const {statusCode, data} = res[1];
+	if(statusCode) {
+		console.info(data.key + "上传成功！");
+		logger.info(data.key + "上传成功！");
+		ctx.response.body = data.key;
+	}else {
+		logger.errors(fileName + "上传失败！");
+		ctx.response.body = '上传失败';
+	}
+}
+
 
 module.exports = {
 	'GET /find': find,
 	'POST /poetry/add': add,
 	'GET /update': update,
-	'GET /remove': remove
+	'GET /remove': remove,
+	'POST /upload': upload
 };
--- src/util/qiniu.js
+++ src/util/qiniu.js
@@ -1,5 +1,6 @@
 const qiniu = require('qiniu');
 const fs = require('fs');
+const tool = require('./tool');
 
 const accessKey = "k2vAtwVFsVSH6kfL40fXk57XzZpxIqnz4uzkfNDN";
 const secretKey = "p5w4k1Cna_d7GBUure5OHqJdbfCJy-20btgkWtcA";
@@ -19,10 +20,23 @@ config.zone = qiniu.zone.Zone_z1;
 var formUploader = new qiniu.form_up.FormUploader(config);
 var putExtra = new qiniu.form_up.PutExtra();
 
+/**
+ * @param  {readableStream} 文件流
+ * @param  {string} 文件标识
+ * @param  {function} 回调函数
+ */
 function uploadQiniu(readableStream, key, callback) {
 	formUploader.putStream(uploadToken, key, readableStream, putExtra, callback);
 }
 
+
+const bucketManager = new qiniu.rs.BucketManager(mac, config);
+const bucket = "music";
+function deleteQiniu(key, callback, bucket="music") {
+	bucketManager.delete(bucket, key, callback);
+}
+
 module.exports = {
-	uploadQiniu: uploadQiniu
+	uploadQiniu: tool.wrap(uploadQiniu),
+	deleteQiniu: tool.wrap(deleteQiniu)
 };
